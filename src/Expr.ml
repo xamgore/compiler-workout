482913167fc64c4710df81e44b76c03292ec7468
xamgore <xamgore@ya.ru>
2018-09-11 15:15:00 +0300

    Complete hometask 1

--- src/Expr.ml
+++ src/Expr.ml
@@ -3,10 +3,10 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-             
-(* The type for the expression. Note, in regular OCaml there is no "@type..." 
-   notation, it came from GT. 
+open GT
+
+(* The type for the expression. Note, in regular OCaml there is no "@type..."
+   notation, it came from GT.
 *)
 @type expr =
   (* integer constant *) | Const of int
@@ -27,28 +27,46 @@ type state = string -> int
 (* Empty state: maps every variable into nothing. *)
 let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-(* Update: non-destructively "modifies" the state s by binding the variable x 
+(* Update: non-destructively "modifies" the state s by binding the variable x
    to value v and returns the new state.
 *)
 let update x v s = fun y -> if x = y then v else s y
 
-(* An example of a non-trivial state: *)                                                   
+(* An example of a non-trivial state: *)
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+    ) ["x"; "a"; "y"; "z"; "t"; "b"] *)
 
 (* Expression evaluator
 
      val eval : state -> expr -> int
- 
-   Takes a state and an expression, and returns the value of the expression in 
+
+   Takes a state and an expression, and returns the value of the expression in
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval state expr =
+  let to_int  b = if b      then 1     else 0    in
+  let to_bool i = if i == 0 then false else true in
+    match expr with
+    | Const (x) -> x
+    | Var (str) -> state str
+    | Binop ("+", x, y) -> (eval state x) + (eval state y)
+    | Binop ("-", x, y) -> (eval state x) - (eval state y)
+    | Binop ("*", x, y) -> (eval state x) * (eval state y)
+    | Binop ("/", x, y) -> (eval state x) / (eval state y)
+    | Binop ("%", x, y) -> (eval state x) mod (eval state y)
+    | Binop ("==", x, y) -> to_int (eval state x == eval state y)
+    | Binop ("!=", x, y) -> to_int (eval state x != eval state y)
+    | Binop ("<=", x, y) -> to_int (eval state x <= eval state y)
+    | Binop (">=", x, y) -> to_int (eval state x >= eval state y)
+    | Binop ("<", x, y) -> to_int (eval state x < eval state y)
+    | Binop (">", x, y) -> to_int (eval state x > eval state y)
+    | Binop ("!!", x, y) -> to_int ((to_bool (eval state x)) || (to_bool (eval state y)))
+    | Binop ("&&", x, y) -> to_int ((to_bool (eval state x)) && (to_bool (eval state y)))
+    | _ -> failwith (Printf.sprintf "Unknown type of expression, can't eval")
